6ff9cbd7f512691b47d95e44295f01abbef4cd39
Kien04840 <kien231204@gmail.com>
2025-09-13 22:52:50 +0700

    update khung dự án, thêm 3 service

---
+++ showtime-service/src/test/java/com/cinebook/showtime/showtime_service/ShowtimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinebook.showtime.showtime_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowtimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
